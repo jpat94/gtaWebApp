b3c6b43829608a7e609d062959d786bee22c4fef
Justin Patterson <34041914+jpat94@users.noreply.github.com>
2021-05-29 00:54:09 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/header/Header';
 import './App.css';
 
-function App() {
+function App () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <body className="card-one">
+        <p className="App-content">Coming soon...</p>
+      </body>
+      <div className="card-two">
+        test
+      </div>
     </div>
   );
 }
